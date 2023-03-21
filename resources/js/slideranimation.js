e4bada7b917085497c9a8c8cc12e7e0cc9968654
francisco-oro <francisco.oro@comunidad.unam.mx>
2023-03-21 12:19:06 -0600

    Improved trends section + sliding carousel

---
+++ resources/js/slideranimation.js
@@ -0,0 +1,7 @@
+// Using pointers to the elements in the featured section 
+const leftarrow=document.getquerySelector('.left-arrow');
+const rightarrow=document.getquerySelector('.right-arrow');
+const slider=document.getquerySelector('.carousel__slider');
+
+// Number of the radio button that is currently selected 
+var cardIndex=document.querySelector('input[name="slide"]:checked');
